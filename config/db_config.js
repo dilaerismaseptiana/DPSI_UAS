2bcebda5929239cf056cb71b740f2d84b32803f1
dilaerismaseptiana <121543024+dilaerismaseptiana@users.noreply.github.com>
2024-07-18 19:23:09 +0700

    fix: repair config env

--- config/db_config.js
+++ config/db_config.js
@@ -9,5 +9,13 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: 'mysql',
   },
+  production: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'mysql',
+  },
   // Add other environments if needed
 };
