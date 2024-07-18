9e6c8feb3049c0971e9b38fe1782cf36cf2ca5a0
dilaerismaseptiana <121543024+dilaerismaseptiana@users.noreply.github.com>
2024-07-18 19:51:27 +0700

    fix: middleware jwt

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,10 +1,11 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config(); // Memuat variabel lingkungan dari .env
 
 const authenticateToken = (req, res, next) => {
   const token = req.headers['authorization'];
   if (!token) return res.sendStatus(401);
 
-  jwt.verify(token.split(' ')[1], 'your_jwt_secret', (err, user) => {
+  jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, user) => {
     if (err) return res.sendStatus(403);
     req.user = user;
     next();
