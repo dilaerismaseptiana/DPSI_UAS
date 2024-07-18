3b19bfac963b29a8f368089dd2aef93da60fa196
dilaerismaseptiana <121543024+dilaerismaseptiana@users.noreply.github.com>
2024-07-18 18:50:06 +0700

    feat: create text

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ const authenticateToken = require('./middleware/authMiddleware');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send('<html><body><h1>API DPSI - Portal Akademik</h1><p>by Dila Erisma</p></body></html>');
 });
 
 module.exports = router;
