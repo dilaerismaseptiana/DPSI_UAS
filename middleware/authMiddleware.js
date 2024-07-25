a6ca527ac4ea24ce6a06e00ae9521acfa5a2f384
dilaerismaseptiana <121543024+dilaerismaseptiana@users.noreply.github.com>
2024-07-25 23:11:53 +0700

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,15 +1,16 @@
 const jwt = require('jsonwebtoken'); // Implementasi JWT
 require('dotenv').config(); // Memuat variabel lingkungan dari .env
 
+// Middleware untuk mengautentikasi token JWT
 const authenticateToken = (req, res, next) => {
-  const token = req.headers['authorization'];
-  if (!token) return res.sendStatus(401);
+  const token = req.headers['authorization']; // Mengambil token dari header Authorization
+  if (!token) return res.sendStatus(401); // Jika tidak ada token, kembalikan status 401 (Unauthorized)
 
   jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
+    if (err) return res.sendStatus(403); // Jika token tidak valid, kembalikan status 403 (Forbidden)
+    req.user = user; // Menyimpan informasi pengguna ke objek req
+    next(); // Melanjutkan ke middleware atau handler berikutnya
   });
 };
 
-module.exports = authenticateToken;
+module.exports = authenticateToken; // Mengekspor middleware untuk digunakan di rute lain
